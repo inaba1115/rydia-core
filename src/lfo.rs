0b4cd8e4417a7f8e57fa2482efcc7f786d3a8da6
inaba1115 <inaba1115@gmail.com>
2026-01-08 06:47:18 +0900

    dev

--- src/lfo.rs
+++ src/lfo.rs
@@ -7,79 +7,84 @@ const P: f32 = 0.225;
 
 fn parabolic_sine(x: f32) -> f32 {
     let y1 = B * x + C * x * x.abs();
-    let y2 = P * (y1 * y1.abs() - y1) + y1;
-    return y2;
+    P * (y1 * y1.abs() - y1) + y1
 }
 
 fn unipolar_to_bipolar(x: f32) -> f32 {
     x * 2.0 - 1.0
 }
 
+#[derive(Clone, Copy)]
+enum LfoWaveform {
+    Sine,
+    Triangle,
+    Saw,
+}
+
+impl From<usize> for LfoWaveform {
+    fn from(v: usize) -> Self {
+        match v {
+            0 => LfoWaveform::Sine,
+            1 => LfoWaveform::Triangle,
+            _ => LfoWaveform::Saw,
+        }
+    }
+}
+
 #[pyclass]
 pub struct Lfo {
-    #[pyo3(get, set)]
-    pub sample_rate: f32,
-
-    #[pyo3(get, set)]
-    pub waveform: usize,
-
-    #[pyo3(get, set)]
-    pub mod_counter: f32,
-
-    #[pyo3(get, set)]
-    pub mod_counter_qp: f32,
+    sample_rate: f32,
+    waveform: LfoWaveform,
+    phase: f32,
+    phase_qp: f32,
 }
 
 #[pymethods]
 impl Lfo {
     #[new]
-    #[pyo3(signature = (sample_rate, waveform=0))]
+    #[pyo3(signature = (sample_rate, waveform = 0))]
     pub fn new(sample_rate: f32, waveform: usize) -> Self {
-        Lfo {
-            sample_rate: sample_rate,
-            waveform: waveform,
-            mod_counter: 0.0,
-            mod_counter_qp: 0.25,
+        assert!(sample_rate > 0.0);
+        Self {
+            sample_rate,
+            waveform: waveform.into(),
+            phase: 0.0,
+            phase_qp: 0.25,
         }
     }
 
     pub fn process(&mut self, frequency: f32) -> (f32, f32) {
         let phase_inc = frequency / self.sample_rate;
 
-        let tmp = unipolar_to_bipolar(self.mod_counter);
-        let tmp_qp = unipolar_to_bipolar(self.mod_counter_qp);
+        let tmp = unipolar_to_bipolar(self.phase);
+        let tmp_qp = unipolar_to_bipolar(self.phase_qp);
 
         let (y, y_qp) = match self.waveform {
-            0 => {
-                // sin
+            LfoWaveform::Sine => {
                 let angle = tmp * PI;
                 let angle_qp = tmp_qp * PI;
-                let y = parabolic_sine(-angle);
-                let y_qp = parabolic_sine(-angle_qp);
-                (y, y_qp)
+                (parabolic_sine(-angle), parabolic_sine(-angle_qp))
             }
-            1 => {
-                // tri
-                let y = 2.0 * tmp.abs() - 1.0;
-                let y_qp = 2.0 * tmp_qp.abs() - 1.0;
-                (y, y_qp)
-            }
-            _ => {
-                // saw
-                (tmp, tmp_qp)
-            }
+            LfoWaveform::Triangle => (2.0 * tmp.abs() - 1.0, 2.0 * tmp_qp.abs() - 1.0),
+            LfoWaveform::Saw => (tmp, tmp_qp),
         };
 
-        self.mod_counter += phase_inc;
-        if self.mod_counter >= 1.0 {
-            self.mod_counter -= 1.0;
-        }
+        self.phase += phase_inc;
+        self.phase -= self.phase.floor();
 
-        self.mod_counter_qp += phase_inc;
-        if self.mod_counter_qp >= 1.0 {
-            self.mod_counter_qp -= 1.0;
-        }
+        self.phase_qp += phase_inc;
+        self.phase_qp -= self.phase_qp.floor();
 
         (y, y_qp)
+    }
+
+    pub fn reset(&mut self) {
+        self.phase = 0.0;
+        self.phase_qp = 0.25;
+    }
+
+    #[getter]
+    fn sample_rate(&self) -> f32 {
+        self.sample_rate
     }
 }
