3805320f5d152c2729582e000dd3a30a5d93e2b6
inaba1115 <inaba1115@gmail.com>
2026-01-08 06:38:40 +0900

    dev

--- examples/foo.py
+++
@@ -1,3 +0,0 @@
-import rydia
-
-print(rydia.sum_as_string(1, 2))
--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,11 @@
-pub mod foo;
 pub mod osc;
 
 use pyo3::prelude::*;
 
-use crate::foo::sum_as_string;
 use crate::osc::{SinOsc, WhiteNoise};
 
 #[pymodule]
 fn rydia(m: &Bound<'_, PyModule>) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
-
     m.add_class::<SinOsc>()?;
     m.add_class::<WhiteNoise>()?;
 
